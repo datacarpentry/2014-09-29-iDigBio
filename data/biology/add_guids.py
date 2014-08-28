26102b8d0b79ae0fb559de39ff3f68f2671bbe8e
Matthew Collins <mcollins@acis.ufl.edu>
2014-08-28 17:14:16 -0400

    Add UUID v4 GUIDs to the portal_mammals.sqlite database; change name of surveys record_id field to survey_id to match its actual meaning

--- data/biology/add_guids.py
+++ data/biology/add_guids.py
@@ -8,12 +8,12 @@ conn = sqlite3.connect("portal_mammals.sqlite")
 # Though this looks horrid it is here to allow debugging without always
 # resetting the database.
 def run_pass(conn, sql):
-	try:
-		conn.execute(sql)
-		conn.commit()
-	except Exception as e:
-		print "Skipped error " + str(e)
-		pass
+    try:
+        conn.execute(sql)
+        conn.commit()
+    except Exception as e:
+        print "Skipped error " + str(e)
+        pass
 
 
 # Add columns to hold new GUIDs
@@ -23,36 +23,44 @@ run_pass(conn, "ALTER TABLE surveys ADD COLUMN survey_guid VAR(36)")
 
 # Generate GUIDs for plots
 for row in conn.execute("SELECT plot_id FROM plots"):
-	plot_id = row[0]
-	plot_guid = uuid.uuid4()
-	conn.execute("UPDATE plots SET plot_guid='%s' WHERE plot_id=%s" %
-		(plot_guid, plot_id))
+    plot_id = row[0]
+    plot_guid = uuid.uuid4()
+    conn.execute("UPDATE plots SET plot_guid='%s' WHERE plot_id=%s" %
+                  (plot_guid, plot_id))
 conn.commit()
 
 # Generate GUIDs for surveys
 for row in conn.execute("SELECT record_id FROM surveys"):
-	record_id = row[0]
-	# expensive, more than a minute
-	#survey_guid = uuid.uuid4()
-	#conn.execute("UPDATE surveys SET survey_guid='%s' WHERE record_id=%s" %
-	#	(survey_guid, record_id))
+    record_id = row[0]
+    # expensive, more than a minute
+    survey_guid = uuid.uuid4()
+    conn.execute("UPDATE surveys SET survey_guid='%s' WHERE record_id=%s" %
+                  (survey_guid, record_id))
 conn.commit()
 
 # Copy tables to new ones to eliminate the old id columns
 run_pass(conn, "DROP TABLE plots2")
+conn.commit()
 run_pass(conn, """CREATE TABLE plots2 AS 
-	SELECT plot_guid AS plot_id, plot_type 
-	FROM plots""")
+                  SELECT plot_guid AS plot_id, plot_type 
+                  FROM plots""")
+conn.commit()
 
 run_pass(conn, "DROP TABLE surveys2")
+conn.commit()
 run_pass(conn, """CREATE TABLE surveys2 AS 
-	SELECT s.survey_guid AS survey_id, s.month, s.day, s.year, 
-		p.plot_guid AS plot_id, s.species, s.sex, s.wgt
-	FROM surveys s LEFT JOIN plots p ON s.plot=p.plot_id""")
+                  SELECT s.survey_guid AS survey_id, s.month, s.day, s.year, 
+                  p.plot_guid AS plot_id, s.species, s.sex, s.wgt
+                  FROM surveys s LEFT JOIN plots p ON s.plot=p.plot_id""")
+conn.commit()
 
-
-
-# ALTER TABLE plots2 RENAME TO plots
-
+run_pass(conn, "DROP TABLE plots")
+conn.commit()
+run_pass(conn, "ALTER TABLE plots2 RENAME TO plots")
+conn.commit()
+run_pass(conn, "DROP TABLE surveys")
+conn.commit()
+run_pass(conn, "ALTER TABLE surveys2 RENAME TO surveys")
+conn.commit()
 
 conn.close()
