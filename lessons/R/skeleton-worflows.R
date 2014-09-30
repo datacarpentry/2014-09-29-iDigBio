8f0e80cb66e11189ba5d65bdd2dfe3ae6156fe17
Francois Michonneau <francois.michonneau@gmail.com>
2014-09-29 20:21:26 -0400

    updating content of workflows lesson

--- lessons/R/skeleton-worflows.R
+++ lessons/R/skeleton-worflows.R
@@ -4,7 +4,7 @@ rename_holothuria <- function(input_path="data/split_files",
                               output_path="data_output/split_files",
                               pattern="(^H\\.|^Hol|^holo)",
                               family="Holothuriidae") {
-    oldNm <- list.files(pattern=pattern, path=path)
+    oldNm <- list.files(pattern=pattern, path=input_path)
     if (length(oldNm) < 1) {
         stop("No file found.")
     }
@@ -12,23 +12,27 @@ rename_holothuria <- function(input_path="data/split_files",
     nm <- cbind(file.path(input_path, oldNm), file.path(output_path, newNm))
     testRnm <- apply(nm, 1, function(x) file.copy(x[1], x[2]))
     if(all(testRnm)) {
-        message("All good.")
+        message("All good!")
+        TRUE
     } else {
         warning("Something went wrong.")
+        FALSE
     }
 }
 
 
 
-collect_holothuria <- function(path="data_output/split_files") {
-    holFiles <- list.files(path)
+collect_holothuria <- function(pattern="^Holothuriidae-", path="data_output/split_files") {
+
+    holFiles <- list.files(path=path, pattern=pattern)
 
     ## create empty data.frame to store the results
     res <- data.frame()
 
     for (i in 1:length(holFiles)) {
         tmpFile <- read.csv(file.path(path, holFiles[i]), stringsAsFactors=FALSE)
-        ## write a command that selects the following commands from tmpFile
+
+        ## write a command that selects the following columns from tmpFile.
         ## dwc.family
         ## dwc.genus
         ## dwc.specificEpithet
